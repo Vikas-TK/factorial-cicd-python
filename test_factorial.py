380f7069a63d1037b7007871fe0240c1da572d0b
Vikas <vikastk.9b@gmail.com>
2026-09-22 22:41:22 +0530

    Add test_factorial

--- test_factorial.py
+++ test_factorial.py
@@ -1,5 +1,8 @@
+import pytest
 from factorial import factorial
-def test_fac():
+def test_fac0():
     assert factorial(0)==1
-def test_fac():
+def test_fac5():
     assert factorial(5)==120
+def test_fac4():
+    assert factorial(4)==24
